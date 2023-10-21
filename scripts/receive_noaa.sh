a61948c74bb90215a0e2460ac86aab705ad560a3
MihajloPi <mihajlo2003petkovic@users.noreply.github.com>
2023-10-21 11:49:39 +0200

    Removing unnecessary white spaces

--- scripts/receive_noaa.sh
+++ scripts/receive_noaa.sh
@@ -133,13 +133,13 @@ else
     bias_tee_option=""
 fi
 
-FLIP="" 
-log "Direction $PASS_DIRECTION" "INFO" 
-if [ "$PASS_DIRECTION" == "Northbound" ]; then 
-  log "I'll flip this image pass because PASS_DIRECTION is Northbound" "INFO" 
-  FLIP="-rotate 180" 
+FLIP=""
+log "Direction $PASS_DIRECTION" "INFO"
+if [ "$PASS_DIRECTION" == "Northbound" ]; then
+  log "I'll flip this image pass because PASS_DIRECTION is Northbound" "INFO"
+  FLIP="-rotate 180"
 fi
- 
+
 # pass start timestamp and sun elevation
 PASS_START=$(expr "$EPOCH_START" + 90)
 export SUN_ELEV=$(python3 "$SCRIPTS_DIR"/tools/sun.py "$PASS_START")
@@ -403,7 +403,7 @@ if [ -n "$(find /srv/images -maxdepth 1 -type f -name "$(basename "$IMAGE_FILE_B
     log "Call pushover script with push_file_list: $push_file_list" "INFO"
     ${PUSH_PROC_DIR}/push_pushover.sh "${pushover_push_annotation}" "${SAT_NAME}" "${push_file_list}"
   fi
-   
+
   # create push annotation string (annotation in the email subject, discord text, etc.)
   # note this is NOT the annotation on the image, which is driven by the config/annotation/annotation.html.j2 file
   push_annotation=""
