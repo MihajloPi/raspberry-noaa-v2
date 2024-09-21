5a3e7c03a810c2d6eba80e39fdc9690eca049478
MihajloPi - YU4MPI <mihajlo.raspberrypi@gmail.com>
2024-09-21 18:18:51 +0200

    Testing Bluesky support

---
+++ scripts/push_processors/push_bluesky.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from atproto import Client
+
+def parse_bluesky_config(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Initialize variable to store the access token
+    bluesky_username = None
+    bluesky_app_password = None
+
+    # Process each line in the file
+    for line in lines:
+        # Remove leading and trailing whitespaces from the line
+        line = line.strip()
+
+        # Skip empty lines or lines starting with a '#' (comments)
+        if not line or line.startswith('#'):
+            continue
+
+        # Split the line into key and value using the '=' separator
+        key, value = line.split('=', 1)
+
+        # Remove leading and trailing whitespaces from the key and value
+        key = key.strip()
+        value = value.strip()
+
+        # Check if the key is 'bluesky_username' or 'bluesky_app_password'
+        if key == 'bluesky_username':
+            bluesky_username = value.strip('\'"')
+        elif key == 'bluesky_app_password':
+            bluesky_app_password = value.strip('\'"')
+
+    return bluesky_username, bluesky_app_password
+
+config_path = os.path.expanduser("~/.bluesky.conf")
+
+BLUESKY_USERNAME, BLUESKY_APP_PASSWORD = parse_bluesky_config(config_path)
+
+SerbianFlag = u'\U0001F1F7' + u'\U0001F1F8'
+
+annotation = sys.argv[1]
+images = []
+for file in sys.argv[2:]:
+  images.append(file)
+
+# split images into groups (Bluesky allows maximum 4 per post)
+if len(images) > 4:
+  images = [ images[i:i+4] for i in range(0, len(images), 4) ]
+else:
+  images = [ images ]
+
+client = Client()
+client.login(BLUESKY_USERNAME, BLUESKY_APP_PASSWORD)
+
+post_text = SerbianFlag + annotation + '\n\n#NOAA #NOAA15 #NOAA18 #NOAA19 #MeteorM2_3 #MeteorM2_4 #weather #weathersats #APT #LRPT #wxtoimg #MeteorDemod #rtlsdr #gpredict #raspberrypi #RN2 #ISS'
+
+for image_group in images:
+  # create post
+  client.send_images(text=post_text, images=image_group)
