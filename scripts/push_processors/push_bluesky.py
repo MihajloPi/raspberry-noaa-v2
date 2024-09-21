02d57d3fe8ccc59c0fb6fa59a08e5d9186b44692
MihajloPi - YU4MPI <mihajlo.raspberrypi@gmail.com>
2024-09-21 20:28:21 +0200

    Fixing BlueSky posting feature
    File size for images unfortunately is limited to 976.56 kB per image.

--- scripts/push_processors/push_bluesky.py
+++ scripts/push_processors/push_bluesky.py
@@ -9,7 +9,7 @@ def parse_bluesky_config(file_path):
         lines = file.readlines()
 
     # Initialize variable to store the access token
-    bluesky_username = None
+    bluesky_username_and_server_instance_url = None
     bluesky_app_password = None
 
     # Process each line in the file
@@ -28,17 +28,17 @@ def parse_bluesky_config(file_path):
         key = key.strip()
         value = value.strip()
 
-        # Check if the key is 'bluesky_username' or 'bluesky_app_password'
-        if key == 'bluesky_username':
-            bluesky_username = value.strip('\'"')
+        # Check if the key is 'bluesky_username_and_server_instance_url' or 'bluesky_app_password'
+        if key == 'bluesky_username_and_server_instance_url':
+            bluesky_username_and_server_instance_url = value.strip('\'"')
         elif key == 'bluesky_app_password':
             bluesky_app_password = value.strip('\'"')
 
-    return bluesky_username, bluesky_app_password
+    return bluesky_username_and_server_instance_url, bluesky_app_password
 
 config_path = os.path.expanduser("~/.bluesky.conf")
 
-BLUESKY_USERNAME, BLUESKY_APP_PASSWORD = parse_bluesky_config(config_path)
+bluesky_username_and_server_instance_url, BLUESKY_APP_PASSWORD = parse_bluesky_config(config_path)
 
 SerbianFlag = u'\U0001F1F7' + u'\U0001F1F8'
 
@@ -54,10 +54,16 @@ else:
   images = [ images ]
 
 client = Client()
-client.login(BLUESKY_USERNAME, BLUESKY_APP_PASSWORD)
+client.login(bluesky_username_and_server_instance_url, BLUESKY_APP_PASSWORD)
 
 post_text = SerbianFlag + annotation + '\n\n#NOAA #NOAA15 #NOAA18 #NOAA19 #MeteorM2_3 #MeteorM2_4 #weather #weathersats #APT #LRPT #wxtoimg #MeteorDemod #rtlsdr #gpredict #raspberrypi #RN2 #ISS'
 
-for image_group in images:
-  # create post
-  client.send_images(text=post_text, images=image_group)
+image_data = []
+for image_path in images:
+  for i in range(0, 4):
+    with open(image_path[i], 'rb') as f:
+      image_data.append(f.read())
+
+#for image_group in images:
+#  # create post
+      client.send_images(text=post_text, images=image_data)
